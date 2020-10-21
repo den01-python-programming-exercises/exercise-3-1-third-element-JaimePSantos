9b6afcfd1797fa04199bba274728fac692fe9c9a
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:11:07 +0000

    title says third, but readme says first. Solution.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    strList = []
+    while(True):
+      str1 = input()
+      if(str1==''):
+        break
+      strList.append(str1)
 
+    print(strList[2])
 if __name__ == '__main__':
     main()
